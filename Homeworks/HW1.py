c5dae27488ed8e5ded149db68c5dfbf61c9c2bcb
İbrahim Utku Uslucan <61324274+UtkuUslucan@users.noreply.github.com>
2021-03-10 01:32:12 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+#GLOBAL AI HUB
+#İbrahim Utku USLUCAN
+#Homework 1
 
-#Question 1
-for x in range(a):
-	print(a)
+
+evenNumbers = list(range(0,20,2))
+print(evenNumbers)
+
+oddNumbers = list(range(1,20,2))
+print(oddNumbers)
+
+commonList = evenNumbers + oddNumbers
+print(commonList)
+
+multiplyList = [i*2 for i in commonList]
+print(multiplyList)
+
+for x in multiplyList:
+    print(type(x))
